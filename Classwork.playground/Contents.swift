ca8dcef80466067e1cc8a3945a39de82dabe1c20
underq <maks.myalicin@gmail.com>
2019-09-22 14:52:56 +0300

    homework

--- Classwork.playground/Contents.swift
+++ Classwork.playground/Contents.swift
@@ -1,6 +1,5 @@
 import UIKit
 
-// 1
 public class Dancer {
     private var name: String
     private var age: Int
@@ -29,19 +28,13 @@ class DancerTango : Dancer {
     }
 }
 
-// 2
+
 enum BinaryTree<T> {
     case empty
     indirect case node(BinaryTree, T, BinaryTree)
 }
 
-// пример построения дерева:
-// левый узел
 let timesLeft = BinaryTree.node(.empty, "2", .empty)
-
-// правый узел
 let timesRight = BinaryTree.node(.empty, "3", .empty)
-
-// корень
 let tree = BinaryTree.node(timesLeft, "1", timesRight)
 
