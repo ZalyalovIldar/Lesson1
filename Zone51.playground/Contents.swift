6ecad103ed8497506e53dd8cd3f864ef06b79f67
Nikita <luzhbinnikita@gmail.com>
2019-09-23 18:51:15 +0300

    Stable Version

--- Zone51.playground/Contents.swift
+++ Zone51.playground/Contents.swift
@@ -1,11 +1,4 @@
-enum ResultOfAttack {
-    
-    case win
-    case lost
-    case bothAlive
-    case bothDead
-    
-}
+//MARK: - Units
 
 class Unit {
     
@@ -16,45 +9,32 @@ class Unit {
     let nickname: String
     
     init(health: Int, damage: Int, protection: Int, agility: Int, nickname: String) {
+        
         self.agility = agility
         self.damage = damage
         self.health = health
         self.nickname = nickname
         self.protection = protection
+        
+        print("\(nickname) enters in the battle")
+        
     }
     
-    func attack(to enemy: inout Unit) -> ResultOfAttack {
+    func attack(to enemy: inout Zona51Guard) -> ResultOfAttack {
         
-        print("")
-        print("\(self.nickname) attacks \(enemy.nickname)")
+        let attackerDamage = self.damage * self.agility
+        let defenderDamage = enemy.damage * enemy.agility
         
-        self.health += self.protection
-        print("Debug: \(self.nickname) had \(self.health) health")
-        self.health -= (enemy.damage * enemy.agility)
-        print("Debug: And now \(self.nickname) have \(self.health) health")
-        
-        enemy.health += enemy.protection
-        print("Debug: \(enemy.nickname) had \(enemy.health) health")
-        enemy.health -= (self.damage * self.agility)
-        print("Debug: And now \(enemy.nickname) have \(enemy.health) health")
+        self.health -= defenderDamage - (defenderDamage * (1 / self.protection))
+        enemy.health -= attackerDamage - (attackerDamage * (1 / enemy.protection))
         
         if self.health <= 0 && enemy.health <= 0 {
-            
-            print("\(self.nickname) and \(enemy.nickname) killed eachother")
-            //print("")
-            
             return .bothDead
         }
-        if enemy.health < 0 {
-            
-            print("\(self.nickname) eliminates \(enemy.nickname)")
-            
+        if enemy.health <= 0 {
             return .win
         }
-        if self.health < 0 {
-            
-            print("\(enemy.nickname) eliminates \(self.nickname)")
-
+        if self.health <= 0 {
             return .lost
         }
         
@@ -63,28 +43,26 @@ class Unit {
     
 }
 
-class Animeshnik: Unit {
-    
+class JoJo: Unit {
 }
 
 class Jedi: Unit {
-    
 }
 
 class ChuckNorris: Unit {
-    
 }
 
 class Zona51Guard: Unit {
-    
 }
+
+//MARK: - Zona51
 
 class ZonaArea {
     
     var isFinished = false
     
-    
-    func beginBattle(attackers: [Unit], defenders: [Unit], wave: Int) -> [Unit] {
+    func beginBattle(attackers: [Unit], defenders: [Zona51Guard], wave: Int) -> [Zona51Guard] {
+        //Returns survived defenders
         
         var attackersInBattle = attackers
         var defendersInBattle = defenders
@@ -94,10 +72,12 @@ class ZonaArea {
             attacker.health += wave * 20
         }
         
-        while !attackersInBattle.isEmpty || !defendersInBattle.isEmpty {
+        while !attackersInBattle.isEmpty && !defendersInBattle.isEmpty {
             
             let currentAttacker = attackersInBattle.randomElement()!
             var currentDefender = defendersInBattle.randomElement()!
+            
+            print("\(currentAttacker.nickname) attacks \(currentDefender.nickname)")
             
             let resultOfAttack = currentAttacker.attack(to: &currentDefender)
             
@@ -108,10 +88,14 @@ class ZonaArea {
                     Unit === currentDefender
                 }) ?? 0)
                 
+                print("\(currentAttacker.nickname) wins")
+                
             case .lost:
                 attackersInBattle.remove(at: attackersInBattle.firstIndex(where: { (Unit) -> Bool in
                     Unit === currentAttacker
                 }) ?? 0)
+                
+                print("\(currentDefender.nickname) wins")
                 
             case .bothDead:
                 defendersInBattle.remove(at: defendersInBattle.firstIndex(where: { (Unit) -> Bool in
@@ -121,10 +105,13 @@ class ZonaArea {
                     Unit === currentAttacker
                 }) ?? 0)
                 
+                print("Both are dead")
+                
             case .bothAlive:
                 print("Both are survived")
 
             }
+            print(" ")
             
         }
 
@@ -134,41 +121,44 @@ class ZonaArea {
     
 }
 
-let attacker1 = Animeshnik(health: 600, damage: 150, protection: 1, agility: 1, nickname: "JoJo")
+enum ResultOfAttack {
+    
+    case win
+    case lost
+    case bothAlive
+    case bothDead
+    
+}
+
+//MARK: - Init Teams
+
+let attacker1 = JoJo(health: 400, damage: 150, protection: 10, agility: 1, nickname: "JoJo")
 let attacker2 = Jedi(health: 300, damage: 50, protection: 50, agility: 3, nickname: "Luke")
 let attacker3 = ChuckNorris(health: 500, damage: 200, protection: 70, agility: 2, nickname: "Chuck Norris")
-
 var attackers = [attacker1, attacker2, attacker3]
 
-let defendersNames = ["John", "Steven", "Mike", "Alfred"]
-var defenders: [Unit] = []
-
-for defenderName in defendersNames {
-    defenders.append(Unit(health: 150, damage: 40, protection: 50, agility: 3, nickname: defenderName))
+var defenders: [Zona51Guard] = []
+for i in 1...10 {
+    defenders.append(Zona51Guard(health: 300, damage: 40, protection: 50, agility: 3, nickname: "Defender #\(i)"))
 }
+
+//MARK: - Start Zona51Battle
 
 let zoneArea = ZonaArea()
 let amountOfWaves = 4
-var survivedDefenders: [Unit] = defenders
-
-var survivedDefendersNames: [String] {
-    var survivedDefendersNames: [String] = []
-    for survivedDefender in survivedDefenders {
-        survivedDefendersNames.append(survivedDefender.nickname)
-    }
-    return survivedDefendersNames
-}
 
 for wave in 1 ... amountOfWaves {
     
-    print("Survived defenders: \(survivedDefendersNames)")
-    
+    print("----------------------------------------")
     print("Wave #\(wave) is started")
-    print("------------------------")
+    print("Survived defenders: \(defenders.count)")
+    print("")
     
-    survivedDefenders = zoneArea.beginBattle(attackers: attackers, defenders: defenders, wave: wave)
+    defenders = zoneArea.beginBattle(attackers: attackers, defenders: defenders, wave: wave)
     
-    if survivedDefenders.isEmpty {
+    if defenders.isEmpty {
+        print("----------------------------------------")
+        print("")
         print("Zone51 was successfully captured")
         break
     } else {
@@ -176,4 +166,3 @@ for wave in 1 ... amountOfWaves {
     }
     
 }
-
