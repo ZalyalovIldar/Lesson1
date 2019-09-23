6585baf9d2b5b8cc9bfb0ea7ad7b47e03de1997f
underq <maks.myalicin@gmail.com>
2019-09-23 10:31:36 +0300

    homework

--- Classwork.playground/Contents.swift
+++ Classwork.playground/Contents.swift
@@ -28,13 +28,87 @@ class DancerTango : Dancer {
     }
 }
 
-
-enum BinaryTree<T> {
-    case empty
-    indirect case node(BinaryTree, T, BinaryTree)
+class Node {
+    
+    let value: Int
+    var left: Node?
+    var right: Node?
+    
+    init(value: Int) {
+        
+        self.value = value
+        left = nil
+        right = nil
+    }
 }
 
-let timesLeft = BinaryTree.node(.empty, "2", .empty)
-let timesRight = BinaryTree.node(.empty, "3", .empty)
-let tree = BinaryTree.node(timesLeft, "1", timesRight)
+class BinaryTree {
+    
+    var root: Node?
+    
+    init(value: Int) {
+        root = Node(value: value)
+    }
+    
+    init() {
+        root = nil
+    }
+    
+    func add(element: Int) {
+        
+        if root == nil {
+            root = Node(value: element)
+        }
+        else {
+            var iterator = root
+            while true {
+                if element > iterator!.value {
+                    if iterator!.right == nil {
+                        iterator!.right = Node(value: element)
+                        return
+                    }
+                    else {
+                        iterator = iterator!.right
+                    }
+                }
+                else {
+                    if iterator!.left == nil {
+                        iterator!.left = Node(value: element)
+                        return
+                    }
+                    else {
+                        iterator = iterator!.left
+                    }
+                }
+            }
+        }
+    }
+    
+    func contains(element: Int) -> Bool {
+        
+        if root == nil {
+            return false
+        }
+        else {
+            var iterator = root
+            while iterator != nil {
+                if element > iterator!.value {
+                    iterator = iterator!.right
+                }
+                else if element < iterator!.value {
+                    iterator = iterator!.left
+                }
+                else {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+}
 
+var tree = BinaryTree()
+tree.add(element: 0)
+tree.add(element: 1)
+print(String(tree.contains(element: 1)))
+print(String(tree.contains(element: 2)))
